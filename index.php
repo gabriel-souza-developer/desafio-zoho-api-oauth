760b4195e26dbc17a0f5ca1958ac3ecf88652af3
gabriel-souza-developer <106005629+gabriel-souza-developer@users.noreply.github.com>
2025-09-02 13:19:58 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Zoho OAuth Teste</title>
+</head>
+<body>
+  <h2>Login com Zoho</h2>
+
+  <<?php 
+  
+
+  ?>
+
+  <a href="<?php echo $auth_url; ?>">
+    <button>Login no Zoho</button>
+  </a>
+</body>
+</html>
